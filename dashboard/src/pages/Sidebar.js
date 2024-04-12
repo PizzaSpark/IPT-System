91aa6439d5bb7ac3e4a695dfad0d3553f4a659ba
Sean <seanpizzaspark@gmail.com>
2024-04-12 16:16:06 +0800

    added logout btn

--- dashboard/src/pages/Sidebar.js
+++ dashboard/src/pages/Sidebar.js
@@ -2,46 +2,54 @@ import React from "react";
 import "./Sidebar.css";
 import { Home, PersonAddAlt1, ViewList } from "@mui/icons-material";
 import { Link } from "react-router-dom";
-
+import {
+    Button,
+} from "@mui/material";
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="items">
-                    <Link to="/dashboard">
-                        <div className="tilecontent">
-                            <Home />
-                            <p>HOME</p>
-                        </div>
-                    </Link>
+                <Link to="/dashboard">
+                    <div className="tilecontent">
+                        <Home />
+                        <p>HOME</p>
+                    </div>
+                </Link>
 
-                    <Link to="/addstudent">
-                        <div className="tilecontent">
-                            <PersonAddAlt1 />
-                            <p>ADD STUDENT</p>
-                        </div>
-                    </Link>
+                <Link to="/addstudent">
+                    <div className="tilecontent">
+                        <PersonAddAlt1 />
+                        <p>ADD STUDENT</p>
+                    </div>
+                </Link>
 
-                    <Link to="/viewstudents">
-                        <div className="tilecontent">
-                            <ViewList />
-                            <p>VIEW STUDENTS</p>
-                        </div>
-                    </Link>
+                <Link to="/viewstudents">
+                    <div className="tilecontent">
+                        <ViewList />
+                        <p>VIEW STUDENTS</p>
+                    </div>
+                </Link>
 
-                    <Link to="/viewusers">
-                        <div className="tilecontent">
-                            <ViewList />
-                            <p>VIEW USERS</p>
-                        </div>
-                    </Link>
+                <Link to="/viewusers">
+                    <div className="tilecontent">
+                        <ViewList />
+                        <p>VIEW USERS</p>
+                    </div>
+                </Link>
 
-                    <Link to="/managestudents">
-                        <div className="tilecontent">
-                            <ViewList />
-                            <p>MANAGE STUDENTS</p>
-                        </div>
-                    </Link>
+                <Link to="/managestudents">
+                    <div className="tilecontent">
+                        <ViewList />
+                        <p>MANAGE STUDENTS</p>
+                    </div>
+                </Link>
+
+                <div className="logout">
+                    <Button variant="contained">LOGOUT</Button>
+                </div>
             </div>
+
+            
         </div>
     );
 }
